7350a474ecef4e5c1e12dc06a3423077263326d2
joaoxg <j.guimaraes@tudelft.nl>
2026-07-23 20:20:25 +0200

    fix interm_rounds and add unique id to images coming fromin GP2 script

--- scripts/GP2_How_did_players_spend_their_money_example.R
+++ scripts/GP2_How_did_players_spend_their_money_example.R
@@ -122,10 +122,14 @@ selected_table <- SELECT_ALL
 selected_cost_types <- SELECT_ALL
 
 ## Retrieve income distribution data frame to be used for data visualization
-income_dist_df <- preprocess_data_list[[selected_gamesession]][[PREPROCESSED_DBTABLES]]
+income_dist_df <- retrieve_GP2_dataframe(preprocess_data_list[[selected_gamesession]][["playerround"]][, INCOME_DIST_ALLCOLS])
+
+round_ids <- get_intermediate_rounds(income_dist_df)
+
+interm_rids <- gsub(ROUND_ACCORDION_IDPREF, "", round_ids[round_ids != SELECT_ALL])
 
 ## Retrieve summary table for data plotted in analysis for GP2
-GP2_summary_df <- retrieve_GP2_summary_tables(income_dist_df, selected_cost_types, selected_table, game_round = SELECT_ALL)$num_df
+GP2_summary_df <- retrieve_GP2_summary_tables(income_dist_df, selected_cost_types, selected_table, game_round = SELECT_ALL, interm_rounds = interm_rids)$num_df
 
 # Export Summary table
 write.csv(GP2_summary_df, file.path(RESULTS_PATH, "GP2_sumstats.csv"), row.names = FALSE, quote = FALSE)
@@ -133,10 +137,11 @@ write.csv(GP2_summary_df, file.path(RESULTS_PATH, "GP2_sumstats.csv"), row.names
 ## Retrieve data to be plotted in analysis for GP2.
 ## Data is retrieved for cost type and table group selection defined above.
 ## Data representative of the whole game session and of each game round is retrieved, respectively.
-GP2_plotall_data <- retrieve_GP2_plot_data(income_dist_df, selected_cost_types, selected_table, game_round = SELECT_ALL, fill_values_all)
-GP2_plot1_data <- retrieve_GP2_plot_data(income_dist_df, selected_cost_types, selected_table, game_round = "1", fill_values_all)
-GP2_plot2_data <- retrieve_GP2_plot_data(income_dist_df, selected_cost_types, selected_table, game_round = "2", fill_values_all)
-GP2_plot3_data <- retrieve_GP2_plot_data(income_dist_df, selected_cost_types, selected_table, game_round = "3", fill_values_all)
+GP2_plotall_data <- retrieve_GP2_plot_data(income_dist_df, selected_cost_types, selected_table, game_round = SELECT_ALL, interm_rounds = interm_rids, fill_values_all)
+GP2_plot1_data <- retrieve_GP2_plot_data(income_dist_df, selected_cost_types, selected_table, game_round = "1", interm_rounds = interm_rids, fill_values_all)
+GP2_plot2_data <- retrieve_GP2_plot_data(income_dist_df, selected_cost_types, selected_table, game_round = "2", interm_rounds = interm_rids, fill_values_all)
+GP2_plot3_data <- retrieve_GP2_plot_data(income_dist_df, selected_cost_types, selected_table, game_round = "3", interm_rounds = interm_rids, fill_values_all)
 
-## Save GP2 plot in main directory and display it in RStudio viewer
-save_and_view_GP2_plot(GP2_plotall_data, file = file.path(RESULTS_PATH, "GP2_plot.png"),  vheight = 1100)
+## Save GP2 plot in main directory and display it in RStudio viewer. Always add date and time to generate new figure
+save_and_view_GP2_plot(GP2_plotall_data, file = file.path(RESULTS_PATH, paste0(format(Sys.time(), "%Y%m%d_%H%M%S"), "_GP2_plot.png")),  vheight = 1100)
+
